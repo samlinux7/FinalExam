cd3bdfb1c13d4c8a327d4be01189f83e701d36ca
samlinux7 <samiansss7@gmail.com>
2025-06-03 11:58:00 +0500

    Completed the logic

--- src/TrafficLightSimmulator.js
+++ src/TrafficLightSimmulator.js
@@ -1,75 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-function TrafficLightSimmulator() {
-    let redLight = true;
-    let greenLight = true;
-    let yellowLight = true;
-
-
-     const trafficLight = [
-        {color: 'red', text: 'RED'},
-        {color: 'yellow', text: 'YELLOW'},
-        {color: 'green', text: 'GREEN'},
-    ];
-
+function TrafficLightSimulator() {
     const [light, setLight] = useState(0);
 
-    
-     setInterval(() => {
-        if(currentLight > 2) {
-            setLight(0);
-            currentLight = 0;
-        } else {
-            setLight(currentLight);
-            currentLight++;
-        }
-    }, 1000);
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setLight(prev => (prev + 1) % 3);
+        }, 1000);
 
-
-    function changeColor1() {
-        this.setState({ color1: 'red', color2: '', color3: '' });
-    };
-
-    function changeColor2() {
-        this.setState({ color1: '', color2: 'yellow', color3: '' });
-    };
-
-    function changeColor3() {
-        this.setState({ color1: '', color2: '', color3: 'green' });
-    };
-
-
-    setInterval(this.changeColor1.bind(this), 1000);
-    setInterval(this.changeColor2.bind(this), 1000);
-    setInterval(this.changeColor3.bind(this), 1000);
-
-    // let color1;
-    // let color2;
-    // let color3;
-
-    // redLight ? color1 = 'red': color1 = 'grey'
-    // greenLight ? color2 = 'green': color2 = 'grey'
-    // yellowLight ? color3 = 'yellow' : color3 = 'grey'
-
-
-
+        return () => clearInterval(interval); 
+    }, []);
 
     return (
         <div className="outer-div">
             <div className="text">
-                Traffic Light Simmulator
+                Traffic Light Simulator
             </div>
 
             <div className="inner-div">
-                <div className="circle1 circle" style={{backgroundColor: trafficLight[light].color}}></div>
-                <div className="circle1 circle" style={{backgroundColor: trafficLight[light].color}}></div>
-                <div className="circle1 circle" style={{backgroundColor: trafficLight[light].color}}></div>
+                <div className="circle1 circle" style={{ backgroundColor: light === 0 ? "red" : "grey" }}></div>
+                <div className="circle1 circle" style={{ backgroundColor: light === 1 ? "yellow" : "grey" }}></div>
+                <div className="circle1 circle" style={{ backgroundColor: light === 2 ? "green" : "grey" }}></div>
             </div>
         </div>
-    )
+    );
 }
 
-
-
-
-export default TrafficLightSimmulator;
+export default TrafficLightSimulator;
