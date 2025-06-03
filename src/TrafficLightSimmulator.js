bfaf0e59a0740d1c5e5b2de3c03fabeed469f3ac
samlinux7 <samiansss7@gmail.com>
2025-06-03 12:06:07 +0500

    Done

--- src/TrafficLightSimmulator.js
+++ src/TrafficLightSimmulator.js
@@ -6,7 +6,7 @@ function TrafficLightSimulator() {
     useEffect(() => {
         const interval = setInterval(() => {
             setLight(prev => (prev + 1) % 3);
-        }, 1000);
+        }, 3000);
 
         return () => clearInterval(interval); 
     }, []);
@@ -18,9 +18,9 @@ function TrafficLightSimulator() {
             </div>
 
             <div className="inner-div">
-                <div className="circle1 circle" style={{ backgroundColor: light === 0 ? "red" : "grey" }}></div>
-                <div className="circle1 circle" style={{ backgroundColor: light === 1 ? "yellow" : "grey" }}></div>
-                <div className="circle1 circle" style={{ backgroundColor: light === 2 ? "green" : "grey" }}></div>
+                <div className="circle1 circle" style={{ backgroundColor: light === 0 ? "red" : "grey", boxShadow: light === 0 ?  `0 0 20px red` :  "0 0 0px, grey"}}></div>
+                <div className="circle1 circle" style={{ backgroundColor: light === 1 ? "yellow" : "grey", boxShadow: light === 1 ?  `0 0 20px yellow` :  "0 0 0px, grey"}}></div>
+                <div className="circle1 circle" style={{ backgroundColor: light === 2 ? "green" : "grey", boxShadow: light === 2 ?  `0 0 20px green` :  "0 0 0px, grey"}}></div>
             </div>
         </div>
     );
