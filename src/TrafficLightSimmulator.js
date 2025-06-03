abf0c4949b68cd6b69e042957546473f829c8ddd
samlinux7 <samiansss7@gmail.com>
2025-06-03 11:05:57 +0500

    UI completed using plain htmls and css

---
+++ src/TrafficLightSimmulator.js
@@ -0,0 +1,17 @@
+function TrafficLightSimmulator() {
+    return (
+        <div className="outer-div">
+            <div className="text">
+                Traffic Light Simmulator
+            </div>
+
+            <div className="inner-div">
+                <div className="circle1 circle"></div>
+                <div className="circle1 circle"></div>
+                <div className="circle1 circle"></div>
+            </div>
+        </div>
+    )
+}
+
+export default TrafficLightSimmulator;
